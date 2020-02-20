7a0e768dc914499c5452fb67246a7186f579444f
Xander Neuwirth <neuwirtha@ad.msoe.edu>
2020-02-20 00:52:53 -0600

    200_200_200_200_af autojob

--- pg.py
+++ pg.py
@@ -7,8 +7,8 @@ from visualizer import get_weight_image
 GAME_BATCH = 10
 MODE = simulator.CUSTOM
 LEARNING_RATE = 0.001
-DENSE_STRUCTURE = (200,200,200)
-ALWAYS_FOLLOW = False
+DENSE_STRUCTURE = (200,200,200,200)
+ALWAYS_FOLLOW = True
 BALL_MARKER_H = False
 BALL_MARKER_V = False
 
